78eb84010027a0bd55b5af49ea24187a94b7e531
Otthon Leão <otthonleao@hotmail.com>
2024-11-16 17:44:10 -0400

    Initial commit

---
+++ src/main/java/dev/otthon/cep/CepAutocompleteApplication.java
@@ -0,0 +1,13 @@
+package dev.otthon.cep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepAutocompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepAutocompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/otthon/cep/CepAutocompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.cep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepAutocompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
