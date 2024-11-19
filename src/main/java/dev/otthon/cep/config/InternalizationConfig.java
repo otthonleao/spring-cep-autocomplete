7df2b8e6776d9282f92b7462560b271c1d89a711
Otthon Leão <otthonleao@hotmail.com>
2024-11-19 01:45:30 -0400

    feat: add Brazilian States with name, uf and IBGE code.

---
+++ src/main/java/dev/otthon/cep/config/InternalizationConfig.java
@@ -0,0 +1,39 @@
+package dev.otthon.cep.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class InternalizationConfig {
+
+    @Bean
+    LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(Locale.getDefault());
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+}
--- src/main/java/dev/otthon/cep/dtos/CustomerDTO.java
+++ src/main/java/dev/otthon/cep/dtos/CustomerDTO.java
@@ -14,31 +14,32 @@ public class CustomerDTO {
 
     private UUID id;
 
-    @NotBlank(message = "O nome não pode ser nulo ou vazio.")
-    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.")
-    @Pattern(regexp = "^[A-Za-zÀ-ÿ ]+$", message = "O nome deve conter apenas letras e espaços.")
+    @NotBlank(message = "{field.required}")
+    @Size(min = 2, max = 100, message = "{field.size.min2max100}")
+    @Pattern(regexp = "^[A-Za-zÀ-ÿ ]+$", message = "{field.name.pattern}")
     private String name;
 
-    @NotBlank(message = "O email é obrigatório e não pode estar vazio.")
-    @Size(max = 255, message = "O email não pode ter mais de 255 caracteres.")
-    @Email(message = "O email deve ser válido, por exemplo: meu_email@dominio.com")
+    @NotBlank(message = "{field.required}")
+    @Size(max = 255, message = "{field.size.max255}")
+    @Email(message = "{field.email.invalid}")
     private String email;
 
-    @NotNull(message = "A data de nascimento é obrigatória.")
-    @Past(message = "A data de nascimento deve estar no passado.")
+    @NotNull(message = "{field.required}")
+    @Past(message = "{field.date.birthdate}")
     private LocalDate birthDate;
 
-    @NotNull(message = "O tipo de cliente é obrigatório: 1 - Pessoa Física, 2 - Pessoa Jurídica.")
-    @Min(value = 1, message = "O tipo de cliente deve ser 1 (Pessoa Física) ou 2 (Pessoa Jurídica).")
-    @Max(value = 2, message = "O tipo de cliente deve ser 1 (Pessoa Física) ou 2 (Pessoa Jurídica).")
+    @NotNull(message = "{field.customer-type.required}")
+    @Min(value = 1, message = "{field.customer-type.invalid}")
+    @Max(value = 2, message = "{field.customer-type.invalid}")
     private Integer customerType;
 
-    @NotNull(message = "O números de telefone é obrigatório.")
-    @Pattern(regexp = "\\d+", message = "O campo deve conter apenas números.")
-    @Size(min = 10, max = 11, message = "O campo deve ter entre 10 e 11 caracteres.")
+    @NotNull(message = "{field.required}")
+    @Positive(message = "{field.numbers.positive}")
+    @Pattern(regexp = "\\d+", message = "{field.numbers.only}")
+    @Size(min = 10, max = 11, message = "{field.numbers.min10max11}")
     private Set<String> phoneNumber;
 
-    @NotNull(message = "O endereço é obrigatório.")
+    @NotBlank(message = "{field.required}")
     private List<Address> addresses;
 
 }
