996f7575b14a523218da1ed35005654a519489f0
Otthon Leão <otthonleao@hotmail.com>
2024-11-23 03:05:38 -0400

    chore: adjust table name `tb_phone_numbers` and attribute `customerType` to `customer_type` for JPA to recognize

--- src/main/java/dev/otthon/cep/entities/Customer.java
+++ src/main/java/dev/otthon/cep/entities/Customer.java
@@ -3,7 +3,6 @@ package dev.otthon.cep.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import dev.otthon.cep.enums.CustomerType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -33,12 +32,12 @@ public class Customer {
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate birthdate;
 
-    @Column(nullable = false)
+    @Column(name = "customer_type", nullable = false)
     private Integer customerType;
 
     @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "phone_numbers", joinColumns = @JoinColumn(name = "customer_id"))
-    @Column(name = "phone_number", nullable = false)
+    @CollectionTable(name = "tb_phone_numbers", joinColumns = @JoinColumn(name = "customer_id"))
+    @Column(name = "tb_phone_number", nullable = false)
     private Set<String> phoneNumber = new HashSet<>();
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
